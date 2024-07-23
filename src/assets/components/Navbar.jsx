9a866a1cd3982e0020da7f1783764537642a75f5
mariab432 <maria.alvisb@campusucc.edu.co>
2024-07-23 11:26:56 -0500

    navbar flexible para inicio de empresas e inicio productos

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,27 +1,41 @@
+import React, { useState } from 'react';
 import { MdOutlineSearch } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
 import { MdShoppingCart } from "react-icons/md";
 import defaultlogo from '/VirtualVentSVG.svg';
+import { FaBars } from "react-icons/fa";
+import "../css/empresas.css";
 
 function Navbar({ logo }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  }
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <img src={logo || defaultlogo} alt="Logo" />
-        </div>
-        <ul className="navbar-links">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="#about">Nosotros</a></li>
-          <li><a href="#contact">Tiendas</a></li>
-        </ul>
-        <ul className="navbar-icons">
-          <li className="icon"><a href=""><MdOutlineSearch /></a></li>
-          <li className="icon"><a href=""><FaRegUser /></a></li>
-          <li className="icon"><a href=""><MdShoppingCart /></a></li>
-        </ul>
-      </nav>
-    </>
+      <>
+        <nav className="navbar">
+          <div className="navbar-logo">
+            <img src={logo || defaultlogo} alt="Logo" />
+          </div>
+          
+          <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="#about">Nosotros</a></li>
+            <li><a href="#contact">Tiendas</a></li>
+          </ul>
+          
+          <button className="nav-toggle" onClick={handleToggle}>
+            <FaBars />
+          </button>
+          
+          <ul className={`navbar-icons ${menuOpen ? 'active' : ''}`}>
+            <li className="icon"><a href=""><MdOutlineSearch /></a></li>
+            <li className="icon"><a href=""><FaRegUser /></a></li>
+            <li className="icon"><a href=""><MdShoppingCart /></a></li>
+          </ul>
+        </nav>
+      </>
   );
 }
 
--- src/assets/components/Shop/ShopNavbar.jsx
+++ src/assets/components/Shop/ShopNavbar.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineSearch } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
 import { MdShoppingCart } from "react-icons/md";
 import defaultlogo from '/VirtualVentSVG.svg';
-import '../../css/shop/shopnavbar.css';
+import { FaBars } from "react-icons/fa";
+import "../../css/empresas.css";
 
 function ShopNavbar({ logo }){
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  }
   return (
-    <div>
+      <>
         <nav className="navbar">
-        <div className="navbar-logo">
-          <img src={logo || defaultlogo} alt="Logo" />
-        </div>
-        <ul className="navbar-links">
-          <li><a href="/">Inicio</a></li>
-          <li><a href="#about">Nosotros</a></li>
-          <li><a href="#contact">Productos</a></li>
-        </ul>
-        <ul className="navbar-icons">
-          <li className="icon"><a href=""><MdOutlineSearch /></a></li>
-          <li className="icon"><a href=""><FaRegUser /></a></li>
-          <li className="icon"><a href=""><MdShoppingCart /></a></li>
-        </ul>
-      </nav>
-    </div>
-  )
+          <div className="navbar-logo">
+            <img src={logo || defaultlogo} alt="Logo" />
+          </div>
+          
+          <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+            <li><a href="/">Inicio</a></li>
+            <li><a href="#about">Nosotros</a></li>
+            <li><a href="#contact">Productos</a></li>
+          </ul>
+          
+          <button className="nav-toggle" onClick={handleToggle}>
+            <FaBars />
+          </button>
+          
+          <ul className={`navbar-icons ${menuOpen ? 'active' : ''}`}>
+            <li className="icon"><a href=""><MdOutlineSearch /></a></li>
+            <li className="icon"><a href=""><FaRegUser /></a></li>
+            <li className="icon"><a href=""><MdShoppingCart /></a></li>
+          </ul>
+        </nav>
+      </>
+  );
 }
 
 export default ShopNavbar
