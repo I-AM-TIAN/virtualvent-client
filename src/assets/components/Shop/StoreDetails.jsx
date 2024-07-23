6ae96f944feb1b3472aeae43871ce63762c5a60b
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-07-23 11:16:39 -0500

    products by shop

--- src/assets/components/Shop/StoreDetails.jsx
+++ src/assets/components/Shop/StoreDetails.jsx
@@ -6,51 +6,25 @@ import ShopNavbar from "./ShopNavbar";
 import ProductList from "./ProductList";
 
 const StoreDetails = () => {
-  const [products, setProducts] = useState([]);
-  const [idCorporativo, setIdCorporativo] = useState(null);
-  const storeURL = window.location.pathname;
-
-  useEffect(() => {
-    const fetchIdCorporativo = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8000/api/corporativo?url=${storeURL}`);
-        setIdCorporativo(response.data.id_corporativo);
-      } catch (error) {
-        console.error('Error fetching id_corporativo:', error);
-      }
-    };
-
-    fetchIdCorporativo();
-  }, [storeURL]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      if (idCorporativo) {
-        try {
-          const response = await axios.get(`http://localhost:8000/api/productos/${idCorporativo}`);
-          setProducts(response.data);
-        } catch (error) {
-          console.error('Error fetching products:', error);
-        }
-      }
-    };
-
-    fetchProducts();
-  }, [idCorporativo]);
-
   const { url } = useParams();
   const [store, setStore] = useState(null);
+  const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get(`http://127.0.0.1:8000/api/store/${url}`)
-      .then((response) => {
-        setStore(response.data);
-      })
-      .catch((error) => {
-        setError("Error fetching store details");
-      });
+    const fetchStoreDetails = async () => {
+      try {
+        const storeResponse = await axios.get(`http://127.0.0.1:8000/api/store/${url}`);
+        setStore(storeResponse.data);
+
+        const productsResponse = await axios.get(`http://127.0.0.1:8000/api/store/${url}/products`);
+        setProducts(productsResponse.data);
+      } catch (error) {
+        setError("Error fetching store details or products");
+      }
+    };
+
+    fetchStoreDetails();
   }, [url]);
 
   if (error) {
